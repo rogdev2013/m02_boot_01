f72349a1fb95b3b31c1ba317e78b1e69b6faf846
rogdevlop <rogdevlop@gmail.com>
2019-04-22 17:27:20 +0200

    mapFilterReduce

---
+++ mapfilterReduce.py
@@ -0,0 +1,17 @@
+#map, filter,reduce
+
+from functools import reduce
+
+lista=[1,3,-1,15,9]
+
+listaDobles = map(lambda x:x*2,lista)
+print(listaDobles)
+
+listaPares = filter(lambda x: x%2==0, lista)
+print(listaPares)
+
+sumatorio = reduce(lambda x,y: x+y,lista)
+print(sumatorio)
+
+suma100 = reduce(lambda x,y: x+y,range(101))
+print(suma100)
