6ddb3d332242b44c17405d794fb378f439d21c80
rogdevlop <rogdevlop@gmail.com>
2019-04-22 18:24:12 +0200

    objetos

---
+++ objetos.py
@@ -0,0 +1,14 @@
+import turtle
+
+tortuguita = turtle.Turtle()
+otraTortuguita = turtle.Turtle()
+
+tortuguita.shape('turtle')
+tortuguita.fd(50)
+tortuguita.color('red')
+
+
+otraTortuguita.shape('turtle')
+otraTortuguita.left(90)
+otraTortuguita.fd(50)
+otraTortuguita.color('green')
