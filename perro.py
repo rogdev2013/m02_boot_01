ad7b1d9b04a450ef2ca4903095307bfa94be2e20
rogdevlop <rogdevlop@gmail.com>
2019-04-23 14:58:48 +0200

    creacion de clases y subclases

--- perro.py
+++ perro.py
@@ -1,3 +1,21 @@
+class Dog():
+    def __init__(self):
+        self.nombre = ""
+        self.edad = None
+        self.peso = None
+    
+    def ladrar(self):
+        if self.peso == None:
+            print("No hay Perro")
+            
+        if self.peso>=8:
+            print("GUAO,GUAO,GUAO")
+        else:
+            print("ay,ay")
+
+
+
+
 class Perro():
     def __init__(self,nombre,edad,peso):
         self.nombre = nombre
@@ -8,4 +26,31 @@ class Perro():
         if self.peso>=8:
             print("GUAO,GUAO,GUAO")
         else:
-            print("guao,guao")
+            print("ay,ay")
+    
+    def __str__(self):
+        return "Perro{},edad:{},peso:{}".format(self.nombre,self.edad,self.peso)
+    
+class PerroAsistencia(Perro):
+    def __init__(self,nombre,edad,peso,amo):
+        Perro.__init__(self,nombre,edad,peso)
+        self.amo = amo
+        self.__trabajando = false
+    
+    def __str__(self):
+        return "Perro de Asistencia de {}".format (self.amo)
+    
+    def ladrar(self):
+        if self.trabajando:
+            print("shhhh, no puedo ladrar")
+        else:
+            Perro.ladrar(self)
+            
+    def trabajando(self,valor=None):
+        if valor == None:
+            return self.__trabajando
+        else:
+            self.__trabajando = valor
+            
+
+    
